c815f95bdc44b6d4ce82d29a7681e68ec4dcc8e0
nathanaeltchamba <natanshost@gmail.com>
2023-05-18 09:58:12 -0500

    updated views and frontend

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import getNotes, getNote, updateNote
+from .views import getNotes, getNote, updateNote, deleteNote
 
 
 urlpatterns = [
@@ -7,7 +7,7 @@ urlpatterns = [
     path('notes/<str:pk>/update', updateNote, name='update-note'),
 
     #path('notes/create/', views.createNote, name="create-note"),
-    #path('notes/<str:pk>/delete/', views.deleteNote, name="delete-note"),
+    path('notes/<str:pk>/delete/', deleteNote, name="delete-note"),
 
     path('notes/<str:pk>/', getNote, name='note-detail'),
 ]
